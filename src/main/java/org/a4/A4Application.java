ffc9d73882afb1fec8bdbed2cd3c6b2548f7207a
Ali <symeri@kth.se>
2018-02-21 22:18:25 +0100

    Initial commit

---
+++ src/main/java/org/a4/A4Application.java
@@ -0,0 +1,12 @@
+package org.a4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A4Application.class, args);
+	}
+}
